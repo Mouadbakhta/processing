0c5027c15e25702dc9b3ef465dc84cbc92c3d90c
Mouadbakhta <m.bakhta3278@uca.ac.ma>
2026-07-15 08:40:57 +0000

    add processing file

--- src/main/java/com/example/processing/service/FileProcessingService.java
+++ src/main/java/com/example/processing/service/FileProcessingService.java
@@ -1,6 +1,33 @@
 package com.example.processing.service;
 
+import com.example.processing.model.ProcessedFile;
+import com.example.processing.model.RawFile;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
+
+@Service
 public class FileProcessingService {
+     private  final ContentNormalizationService normalized ;
+     private  final DuplicateDetectionService duplicate ;
+     private final FilePersistenceService persistence;
+
+     public ProcessedFile process(RawFile file) throws NoSuchAlgorithmException {
+         ProcessedFile processedFile = normalized.normalize(file);
+         processedFile = duplicate.vifrefy(processedFile);
+
+         processedFile = persistence.save(processedFile);
+
+         return  processedFile;
+     }
 
 
+    public FileProcessingService(ContentNormalizationService normalized, DuplicateDetectionService duplicate, FilePersistenceService persistence) {
+        this.normalized = normalized;
+        this.duplicate = duplicate;
+        this.persistence = persistence;
+    }
 }
